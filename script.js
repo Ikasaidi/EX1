d1a9b33d3e48a7ef5277746f9c3e05f6d35e5e5c
Ikram Saidi <6250772@collegemv.qc.ca>
2024-02-07 16:43:42 -0500

    hi

--- script.js
+++ script.js
@@ -1,38 +1,8 @@
-// /* javascript.js */
-// const taskInput = document.getElementById("taskInput") ;
-// const addTaskBtn = document.getElementById("addTaskBtn");
-// const taskList = document.getElementById("tasklist") ;
-
-// addTaskBtn.addEventListener("click", addTask);
-
-// function addTask() {
-//     const taskText = taskInput.value.trim();
-
-// // La fonction trim() en JavaScript est utilisée pour supprimer les espaces (espaces, tabulations et nouvelles lignes) au début et à la fin d'une chaîne.
-//     if (taskText !== "") {
-//         const listItem = document.createElement("li");
-//         listItem.textContent = taskText;
-//         taskList.appendChild(listItem);
-//         taskInput.value = "";
-//         // Ajoutez un bouton de suppression à chaque élément de liste
-//         const deleteBtn = document.createElement("button");
-//         deleteBtn.textContent = "Supprimer";
-
-// // La méthode JavaScript appendChild() est utilisée pour insérer un nouveau nœud ou repositionner un nœud existant en tant que dernier enfant d'un nœud parent particulier.
-//         listItem.appendChild(deleteBtn);
-    
-//         deleteBtn.addEventListener("click", () => {
-//         listItem.remove();
-//         });
-//     } else {
-//         alert("Veuillez entrer une tâche valide.");
-//     }
-// }
-
 const taskInput = document.getElementById("taskInput") ;
 const addTaskBtn = document.getElementById("addTask") ;
 const taskList = document.getElementById("taskList") ;
- 
+var task = "task";
+var i = 0 ;
 // La fonction trim() en JavaScript est utilisée pour supprimer les espaces (espaces, tabulations et
 //nouvelles lignes) au début et à la fin d'une chaîne.
  
@@ -56,4 +26,7 @@ function addTask() {
     alert("Veuillez entrer une tâche valide.");
  
  }
+ localStorage.setItem(task.concat(i), taskText);
+    i++;
+  
  }
